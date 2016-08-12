f19ed959333e3f40a2867bd9e0d74d4b8457cc58
ly444983 <410444983@qq.com>
2016-08-12 17:50:46 +0800

    1

--- ItChat_do.py
+++ ItChat_do.py
@@ -20,20 +20,23 @@ def music_player(msg):
         return llcx("8986061609000073005")
     
 def car():
-    cookies = {"JSESSIONID": "F91124FB808BFA61AF8C8321BE90FAEB"}
     headers = \
         {'User-Agent': 'Dalvik/2.1.0 (Linux; U; Android 6.0.1; MI 3W MIUI/V7.5.6.0.MXDCNDE)',
-         'encryptValue': '4Z9u1KJRrJU='
+         'encryptValue': '4Z9u1KJRrJU=',
+         'host':'admin.bjev520.com'
          }
     data = {"id": "14469"}
-    data_string = requests.post("http://admin.bjev520.com/jsp/interface/statusCharging/do/statusCharging.jsp",
-                                data=data, headers=headers).text
-
+    data_string = requests.post("http://121.42.144.98/jsp/interface/statusCharging/do/statusCharging.jsp",data=data, headers=headers).text
     data_string = json.loads(data_string)["data"]
     return (json.dumps(data_string).replace(",", "\n"))
+
 def llcx(a):
+    headers = \
+        {'User-Agent': 'Dalvik/2.1.0 (Linux; U; Android 6.0.1; MI 3W MIUI/V7.5.6.0.MXDCNDE)',
+         'host':'10010.mahalian.cn'
+         }
     try:
-        data_string=requests.get("http://10010.mahalian.cn/Home/Index/index?search_id="+a).text
+        data_string=requests.get("http://123.56.92.247/Home/Index/index?search_id="+a, headers=headers).text
         data_string=json.loads(data_string)["result"]["left_usage"]
     except:
         return "!"
