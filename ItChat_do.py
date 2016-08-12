a5d721e604b79666d35fb54833a2e4987a021aa4
ly444983 <410444983@qq.com>
2016-08-12 15:54:06 +0800

    1

--- ItChat_do.py
+++ ItChat_do.py
@@ -1,11 +1,30 @@
 #coding=utf8
-import itchat,os,random
+import itchat,json,requests
 import threading
 
 def login():
 	itchat.auto_login(hotReload = True)
 	itchat.run()
 
+@itchat.msg_register('Text')
+def music_player(msg):
+	if msg['FromUserName'] != msg['ToUserName']:
+		return
+#		if itchat.__client.web_init()["UserName"]!=msg['ToUserName']:
+	if msg['Text'].isdigit() :
+		if 898606160900007300 < int(msg['Text']) < 89860616090000730050:
+			return llcx(msg['Text'])
+	elif msg['Text']=="cxll":
+		return llcx("8986061609000073005")
+
+def llcx(a):
+	try:
+		data_string=requests.get("http://10010.mahalian.cn/Home/Index/index?search_id="+a).text
+		data_string=json.loads(data_string)["result"]["left_usage"]
+	except:
+		return "!"
+	else:
+		return str(data_string)
 
 t1 = threading.Thread(target=login,args=())
 t1.setDaemon(True)
--- server.py
+++ server.py
@@ -10,49 +10,55 @@ PORT = int(os.getenv('PORT', 80))
 
 @app.route('/senda/<a>')
 def senda(a):
-	try:
-		itchat.send(request.META.get("REMOTE_ADDR")+","+a+","+request.headers.get('User-Agent'),itchat.__client.web_init()["UserName"])
-		return "OK"
-	except Exception as e:
-		return str(e)
+    try:
+        itchat.send(request.META.get("REMOTE_ADDR")+","+a+","+request.headers.get('User-Agent'),itchat.__client.web_init()["UserName"])
+        return "OK"
+    except Exception as e:
+        return str(e)
 @app.route('/')
 def index():
-	if os.path.exists('QR.jpg'):
-		itchat.__client.showedQR=1
-		return send_from_directory("","QR.jpg")
-	return itchat.__client.web_init()["NickName"]
+    if os.path.exists('QR.jpg'):
+        itchat.__client.showedQR=1
+        return send_from_directory("","QR.jpg")
+    return itchat.__client.web_init()["NickName"]
 @app.route('/llcx')
 def llcx():
-	a='8986061609000073005'
-	try:
-		data_string=requests.get("http://10010.mahalian.cn/Home/Index/index?search_id="+a).text
-		
-		data_string=json.loads(data_string)["result"]["left_usage"]
-	except:
-		return render_template('llcx2.html',t="!")
-	else:
-		return render_template('llcx2.html',t=data_string)
+    a='8986061609000073005'
+    try:
+        data_string=requests.get("http://10010.mahalian.cn/Home/Index/index?search_id="+a).text
+        
+        data_string=json.loads(data_string)["result"]["left_usage"]
+    except:
+        return render_template('llcx2.html',t="!")
+    else:
+        return render_template('llcx2.html',t=data_string)
 
 @app.route('/favicon.ico')
 def favicon():
     return send_from_directory("templates",'favicon.ico')
+
 @app.route('/rn')
 def rn():
     return send_from_directory("templates",'rnWatcher.html')
 
 @app.route('/d')
 def d():
-	t1 = threading.Thread(target=dAs,args=(request.args.get('url'),))
-	t1.setDaemon(True)
-	t1.start()
-	return "ok",200
-
+    t1 = threading.Thread(target=dAs,args=(request.args.get('url'),))
+    t1.setDaemon(True)
+    t1.start()
+    return "ok",200
+global a
+a=[]
 def dAs(url):
-    print (url)
-    local_filename = url.split('/')[-1]  
-    r = requests.get(url, stream=True) # here we need to set stream = True parameter  
+    local_filename = url.split('/')[-1]
+    if local_filename in a:
+        print(local_filename+" is downloaded")
+        return
+    a.append(local_filename)
+    r = requests.get(url, stream=True)
     with open(local_filename, 'wb') as f: f.write(r.content)
     itchat.send('@img@'+local_filename)
+    os.remove(local_filename)
 
 
 app.run(host='0.0.0.0',port=PORT)
