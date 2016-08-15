ee37311e0d71d9c4dd4ef0aedd0353da7cd84f0b
ly444983 <410444983@qq.com>
2016-08-15 16:13:11 +0800

    1

--- server.py
+++ server.py
@@ -79,9 +79,9 @@ def car():
     data_string = requests.post("http://121.42.144.98/jsp/interface/statusCharging/do/statusCharging.jsp",data=data, headers=headers).text
     data_string = json.loads(data_string)["data"]
     t=int(data_string["travelTimeDate"])/1000+8*60*60
-
     data_string["travelTimeDate"]= time.strftime('%y-%m-%d %H:%M:%S',time.gmtime(t))
     return format(json.dumps(data_string))
+
 t1 = threading.Thread(target=keep,args=())
 t1.setDaemon(True)
 t1.start()
